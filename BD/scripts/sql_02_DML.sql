df5f79f5d999c6ac25c4b5c60ca9f3db0872aab2
Angelo Gabriel Vieira Rodrigues <agvr74@gmail.com>
2022-02-25 07:45:51 -0300

    DML Finalizado

--- BD/scripts/sql_02_DML.sql
+++ BD/scripts/sql_02_DML.sql
@@ -7,11 +7,15 @@ VALUES
 	('Aluno'),
 	('Professor')
 GO
+--SELECT * FROM tipoUsuario
+--GO
 
 INSERT INTO instituicao(nomeInstituicao, numeroInstituicao, enderecoInstituicao)
 VALUES
 	('SESI', '081', 'Av. Sen. Roberto Símonsen, 550 - Jardim Imperador, Suzano - SP, 08673-270')
 GO
+--SELECT * FROM instituicao
+--GO
 
 INSERT INTO usuario(idInstituicao, idTipoUsuario, nomeUsuario, rg, email, senha, imagem)
 VALUES
@@ -19,20 +23,42 @@ VALUES
 	('1', '2', 'alunoteste', '23.456.789-0', 'aluno@email.com', 'aluno123', ''),
 	('1', '3', 'professorteste', '34.567.890-1', 'professor@email.com', 'professor123', '')
 GO
+--SELECT * FROM usuario
+--GO
 
 INSERT INTO professor(idUsuario, matricula)
 VALUES
 	('3', '1234')
 GO
+--SELECT * FROM professor
+--GO
 
 INSERT INTO periodo(nomePeriodo)
 VALUES
 	('Manhã'),
 	('Tarde')
 GO
+--SELECT * FROM periodo
+--GO
 
 INSERT INTO turma(idPeriodo, nomeTurma)
 VALUES
 	('1', '3°Ano'),
 	('2', '3°Ano')
 GO
+--SELECT * FROM turma
+--GO
+
+INSERT INTO aluno(idTurma, idUsuario, matricula)
+VALUES
+	('1', '2', '112233')
+GO
+--SELECT * FROM aluno
+--GO
+
+INSERT INTO cracha(idUsuario, token)
+VALUES
+	('2', '11223344556677889900')
+GO
+--SELECT * FROM cracha
+--GO
